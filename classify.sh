8e75178c5b955012e14f262f8502492b1f97f9bd
jiangkai <airborne08@gmail.com>
2012-09-14 14:26:07 +0800

    update to recursive

--- classify.sh
+++ classify.sh
@@ -4,23 +4,34 @@ usage(){
 	echo -e "Usage:test.sh arg"
 	echo -e "And arg must be directory"
 }
-DIR_PATH=$1
-if [ $# -eq 1 ] && [ -d $1 ]; then
-	for file in $(ls $DIR_PATH); do
-		if test -d $DIR_PATH$file;then
-			echo $DIR_PATH$file " is directory"
+
+classify(){
+	if echo $1 | grep "/$" > /dev/null; then
+		ABS_PATH=$1
+	else
+		ABS_PATH=$1/
+	fi
+	#echo "entering " $ABS_PATH
+	for file in $(ls $ABS_PATH); do
+		if test -d $ABS_PATH$file;then
+			echo $ABS_PATH$file " is directory"
+			$0 $ABS_PATH$file
 		else
-			if grep -e "^#\!/.*/*sh" $DIR_PATH$file > /dev/null; then
-				echo $DIR_PATH$file "is a shell script"
-			elif grep -e "^#\!/.*/*python" $DIR_PATH$file > /dev/null; then
-				echo $DIR_PATH$file "is a python script"
-			elif file $DIR_PATH$file | grep "ELF" > /dev/null; then
-				echo $DIR_PATH$file "is a ELF executable file"
+			if grep -e "^#\!/.*/*sh" $ABS_PATH$file > /dev/null; then
+				echo $ABS_PATH$file "is a shell script"
+			elif grep -e "^#\!/.*/*python" $ABS_PATH$file > /dev/null; then
+				echo $ABS_PATH$file "is a python script"
+			elif file $ABS_PATH$file | grep "ELF" > /dev/null; then
+				echo $ABS_PATH$file "is a ELF executable file"
 			else
-				echo $DIR_PATH$file "not recognized, may be ascii text?"
+				echo $ABS_PATH$file "not recognized, may be ascii text?"
 			fi
 		fi
 	done
+}
+DIR_PATH=$1
+if [ $# -eq 1 ] && [ -d $1 ]; then
+	classify "$DIR_PATH"
 else
 	usage
 
